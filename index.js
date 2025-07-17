c3be697632740b96038a695798c9e8e002999ade
Philip Knapp <philipsknapp@gmail.com>
2025-07-17 14:18:47 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors');
-
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
